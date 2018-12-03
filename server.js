9f140598cb383751047e88de24daaf79df5129c9
Jefek <jeffrey.knowles@gmail.com>
2018-12-03 12:43:57 -0500

    Added ejs variant

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ console.log("Let's find out what express is", express);
 var app = express();
 console.log("Let's find out what app is", app);
 // use app's get method and pass it the base route '/' and a callback
-app.get('/', function(request, response) {
-    // just for fun, take a look at the request and response objects
-   console.log("The request object", request);
-   console.log("The response object", response);
-   // use the response object's .send() method to respond with an h1
-   response.send("<h1>Hello Express</h1>");
-})
+// app.get('/', function(request, response) {
+//     // just for fun, take a look at the request and response objects
+//    console.log("The request object", request);
+//    console.log("The response object", response);
+//    // use the response object's .send() method to respond with an h1
+//    response.send("<h1>Hello Express</h1>");
+// })
+// removed index route to test routing
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
   console.log("listening on port 8000");
@@ -37,4 +38,12 @@ app.get("/users", function (request, response){
         {name: "Andrew", email: "andrew@codingdojo.com"}
     ];
     response.render('users', {users: users_array});
+})
+
+app.get("/cats", function (request, response){
+    response.render('cats');
+})
+
+app.get("/cars", function (request, response){
+    response.render('cars');
 })
